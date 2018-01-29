96501e78c87d648581b01a1e848f9dca028246c6
brosell1 <brosell@live.co.uk>
2018-01-29 13:36:05 +0000

    added vertical curtain for mobile

--- src/components/Curtains/index.js
+++ src/components/Curtains/index.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 
+import MediaQuery from "react-responsive";
+
 import CurtainLeft from "../CurtainLeft";
 import CurtainRight from "../CurtainRight";
+import CurtainVert from "../CurtainVert";
 
 class Curtains extends Component {
 	render() {
 		return (
 			<div>
-				<CurtainLeft curtains={this.props.curtains} />
-				<CurtainRight curtains={this.props.curtains} />
+				<MediaQuery query="(min-device-width: 1224px)" >
+					<CurtainLeft curtains={this.props.curtains} />
+					<CurtainRight curtains={this.props.curtains} />
+				</MediaQuery>
+				<MediaQuery query="(max-device-width: 1224px)" >
+					<CurtainVert curtains={this.props.curtains} />
+				</MediaQuery>
 			</div>
 		);
 	}
---
+++ src/components/TitleTextVert/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "./style.css"
+
+class TitleTextLeft extends Component {
+	render() {
+		return (
+			<div className="titleTextVert">
+				<h1>Ben is a<br />
+				full-stack<br />
+				Javascript<br />
+				Developer</h1>
+			</div>
+		);
+	}
+}
+
+export default TitleTextLeft;
